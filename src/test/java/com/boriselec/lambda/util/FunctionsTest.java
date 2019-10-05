e6922a687b7d1d5687bdcae6c2b72093721cf9d6
boris.puzikov <boris.puzikov@gmail.com>
2019-10-05 15:17:34 +0300

    predecessor logic if condition

--- src/test/java/com/boriselec/lambda/util/FunctionsTest.java
+++ src/test/java/com/boriselec/lambda/util/FunctionsTest.java
@@ -1,25 +1,63 @@
 package com.boriselec.lambda.util;
 
+import com.boriselec.lambda.Var;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.boriselec.lambda.util.Functions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FunctionsTest {
     @Test
     void testSuccessor() {
-        assertEquals(Number.get(3), Functions.S.apply(Number.get(2)),
+        assertEquals(Number.get(3), S.apply(Number.get(2)),
                 "3 == ++2");
     }
 
     @Test
+    void testPredecessor() {
+        assertEquals(Number.get(2), P.apply(Number.get(3)),
+                "2 == --3");
+        assertEquals(Number.get(0), P.apply(Number.get(0)),
+                "0 == --0");
+    }
+
+    @Test
     void testAddition() {
-        assertEquals(Number.get(5), Number.get(2).apply(Functions.S).apply(Number.get(3)),
+        assertEquals(Number.get(5), Number.get(2).apply(S).apply(Number.get(3)),
                 "5 == 2 + 3");
     }
 
     @Test
     void testMultiplication() {
-        assertEquals(Number.get(1200), Functions.M.apply(Number.get(40)).apply(Number.get(30)),
+        assertEquals(Number.get(1200), M.apply(Number.get(40)).apply(Number.get(30)),
                 "1200 == 40 * 30");
+    }
+
+    @Test
+    void testAnd() {
+        assertEquals(T, AND.apply(T).apply(T),
+                "true == true and false");
+        assertEquals(F, AND.apply(T).apply(F),
+                "false == true and false");
+        assertEquals(F, AND.apply(F).apply(T),
+                "false == false and true");
+        assertEquals(F, AND.apply(F).apply(F),
+                "false == false and false");
+    }
+
+    @Test
+    void testIf() {
+        assertEquals(new Var('i'), IF_THEN_ELSE.apply(T).apply(new Var('i')).apply(new Var('j')),
+                "i == if (true) then i else j");
+        assertEquals(new Var('j'), IF_THEN_ELSE.apply(F).apply(new Var('i')).apply(new Var('j')),
+                "j == if (false) then i else j");
+    }
+
+    @Test
+    void testIsZero() {
+        assertEquals(T, IS_ZERO.apply(Number.get(0)),
+                "true == (0 == 0)");
+        assertEquals(F, IS_ZERO.apply(Number.get(3)),
+                "false == (3 == 0)");
     }
 }
